189998429f7d821245ac85e01e18269d6b7a4cc9
Maksim Kuklin <mqklin@gmail.com>
2022-04-03 19:03:16 +0500

    save

--- src/createImage/index.js
+++ src/createImage/index.js
@@ -5,9 +5,16 @@ import fs from 'fs';
 import path from 'path';
 import {convert} from 'convert-svg-to-png';
 
+const gray20 = '#7A859E';
+const dragon = '#65B67D';
+const phoenix = '#E4645B';
+const gray70 = '#1E2028';
+
 export async function createImage({forecast, signalsContractAddress, imagePath}) {
   const width = 1200;
   const height = 630;
+  const margin = 32;
+  console.log(forecast);
 
   const {signalOpenDateIndex, signalCloseDateIndex, prices} = await getChartData(forecast, timeframe_15_min);
 
@@ -21,15 +28,22 @@ export async function createImage({forecast, signalsContractAddress, imagePath})
   const maxShiftedDate = Math.max(...shiftedChartPoints.map(([x]) => x));
   const maxShiftedPrice = Math.max(...shiftedChartPoints.map(([, y]) => y));
 
-  const points = shiftedChartPoints.map(([x, y]) => [x / maxShiftedDate * width, y / maxShiftedPrice * height]);
+  const points = shiftedChartPoints.map(([x, y]) => [x / maxShiftedDate * width, height - y / maxShiftedPrice * height]);
+  const marginPoints = points.map(([x, y]) => [(x - width / 2) * (width / 2 - margin) / (width / 2) + width / 2, (y - height / 2) * (height / 2 - margin) / (height / 2) + height / 2]);
 
   const svg = `
     <svg width=${width} height=${height} preserveAspectRatio="none" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
       <rect width="100%" height="100%" fill="white"/>
       <polyline
         fill="none"
-        points="${points.map(([x, y]) => `${x},${height - y}`).join(' ')}"
-        stroke="#7A859E"
+        points="${marginPoints.map(([x, y]) => `${x},${y}`).join(' ')}"
+        stroke=${gray20}
+        strokeWidth="1"
+      />
+      <polyline
+        fill="none"
+        points="${marginPoints.slice(signalOpenDateIndex, signalCloseDateIndex || undefined).map(([x, y]) => `${x},${y}`).join(' ')}"
+        stroke="${signalCloseDateIndex === null ? gray70 : forecast.performance > 0 ? dragon : phoenix}"
         strokeWidth="1"
       />
     </svg>
