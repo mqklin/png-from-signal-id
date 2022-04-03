ddc74ad38e9522f59ad7f2ec64a394f48093c252
Maksim Kuklin <mqklin@gmail.com>
2022-04-03 17:01:54 +0500

    save

--- src/createImage/index.js
+++ src/createImage/index.js
@@ -1,29 +1,18 @@
 /* eslint-disable import/no-nodejs-modules */
 
-import ReactDOMServer from 'react-dom/server';
-
 import getChartData, {timeframe_15_min} from 'App/utils/getChartData';
 import fs from 'fs';
 import path from 'path';
 import {convert} from 'convert-svg-to-png';
 
 export async function createImage({forecast, signalsContractAddress, imagePath}) {
-  const contentWidth = 1800;
-  const contentMargin = 16;
-  const bodyWidth = contentWidth + contentMargin * 2;
-  const chartHeight = 640;
 
   const chartData = await getChartData(forecast, timeframe_15_min);
+  console.log(chartData);
 
   const svg = `
-    <svg width="100" height="100" preserveAspectRatio="none" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <g>
-        <g>
-          <svg height="18" width="100">
-            <text alignmentBaseline="middle" textAnchor="middle" x="50%" y="50%">Performance</text>
-          </svg>
-        </g>
-      </g>
+    <svg width="1200" height="630" preserveAspectRatio="none" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <rect width="100%" height="100%" fill="white"/>
     </svg>
   `.replace(/\n/g, '');
   fs.writeFileSync(imagePath, await convert(svg));
